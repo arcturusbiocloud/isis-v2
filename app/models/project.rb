cb2c613a8f57c5b83aa58d81a17becb5f8badc60
Bruno Freitas <bmsatierf@gmail.com>
2015-04-03 21:57:11 -0300

    Add scope for active projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,11 +6,13 @@ class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  # A project is created with status 0 (pending); then another process grab the
-  # project, allocate resources for it and change the status to 1 (running).
-  # Lastly, that same process should finish the project, changing its status
-  # to 2 (completed)
-  enum status: { pending: 0, running: 1, completed: 2 }
+  # A project is created with status 0 (pending); then the background job grabs
+  # the project, allocate resources for it and change the status to 1 (running).
+  # During the running phase, the robot performs various tasks, and at a certain
+  # point, the status is changed again, and the new status (2) indicates the
+  # project is incubating and ready to be photographed. After 36 pictures, the
+  # status is changed to 3 indicating that it is completed.
+  enum status: { pending: 0, running: 1, incubating: 2, completed: 3 }
 
   validates :name,
             presence: true,
@@ -25,6 +27,20 @@ class Project < ActiveRecord::Base
 
   # Projects qualified to be shown on /explore
   scope :is_public, -> { where("is_public = 'true'") }
+
+  # Projects incubating where the last picture was taken one hour ago and
+  # with less than 36 pictures in total.
+  # Note that the having clause is using count < 41, that's because before
+  # stating taking picture, five other activities happens, so 5 + 36 = 41.
+  scope :active, -> {
+    select("projects.*, COUNT(activities.id)").
+     joins("JOIN activities ON activities.project_id = projects.id").
+     where("projects.status = 2").
+     where("projects.last_picture_taken_at > NOW() - INTERVAL '60 minutes'
+            OR projects.last_picture_taken_at IS NULL").
+     group("projects.id").
+    having("COUNT(activities.id) < 41")
+  }
 
   before_create :random_icon
 
