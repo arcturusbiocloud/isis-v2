f28128592d3e54f5fa0b4abe5d651ef53db71235
Bruno Freitas <bmsatierf@gmail.com>
2015-04-06 23:18:19 -0300

    Root should be home#index

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   match ':username' => 'projects#index', via: :get, as: 'username_projects'
   match ':username/:id' => 'projects#show', via: :get, as: 'username_project'
 
-  root 'explore#index'
+  root 'home#index'
 end
