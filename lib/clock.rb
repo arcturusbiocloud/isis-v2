3e0617d2baa8937ce3b4a08289ea3132ca15bc11
luisbebop <luisbebop@gmail.com>
2015-04-24 19:55:22 -0700

    Changed clockwork to call the virtual experiment endpoint.

--- lib/clock.rb
+++ lib/clock.rb
@@ -17,17 +17,21 @@ every(1.minute, 'Trying to run experiment') {
 
     # making the rest call to run the experiment
     puts "making rest call to run the experiment :project_id => #{project.id}, :slot => #{free_slot}, :genetic_parts => #{project.genetic_parts.to_json}"
-    # req = RestClient.post "http://arcturus:huxnGrbNfQFR@#{url}/api/run_virtual_experiment", :project_id => project.id, :slot => free_slot, :genetic_parts => project.genetic_parts.to_json
-    # puts req
-
+    req = RestClient.post "http://arcturus:huxnGrbNfQFR@#{url}/api/run_virtual_experiment", :project_id => project.id, :slot => free_slot, :genetic_parts => project.genetic_parts.to_json
     # compare req and save the slot on the project 
+    unless req.nil?
+      status = JSON.parse(req)["status"]
+      if status != nil && status != "error"
+        project.slot = free_slot
+        project.save
+      end
+    end
+    puts req
   end
 }
 
 every(1.minute, 'Trying to take a picture') {
-  # take picture with Project.active.last
-  # change the Project model to update the status of project to completed after 3 pictures
-  # ...
+  # TODO: change the Project model to update the status of project to completed after 3 pictures
   project = Project.active.last
   unless project.nil?
     puts "making rest call to take the picture :project_id=> #{project.id}, :slot => #{project.slot}"
