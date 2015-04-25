9e1c2feaa5be8e73e164ce3f26d56072eb996dc5
luisbebop <luisbebop@gmail.com>
2015-04-24 19:11:29 -0700

    Adding clockwork test calls to handle the experiment execution and picture take

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,8 +5,31 @@ require 'rest-client'
 
 include Clockwork
 
-every(5.seconds, 'Calling robots') { 
+every(1.minute, 'Trying to run experiment') {
+  # define the API url
   url = Rails.env.production? ? "horus01.arcturus.io:3000" : "10.1.10.111:3000"
-  puts "url=#{url}"
-  puts RestClient.get "http://arcturus:huxnGrbNfQFR@#{url}/api/online"
+  
+  # execute a pending experiment
+  project = Project.pending.last
+  unless project.nil?
+    # get the free slot to run the experiment
+    free_slot = Project.free_slot
+
+    # making the rest call to run the experiment
+    puts "making rest call to run the experiment :project_id => #{project.id}, :slot => #{free_slot}, :genetic_parts => #{project.genetic_parts.to_json}"
+    # req = RestClient.post "http://arcturus:huxnGrbNfQFR@#{url}/api/run_virtual_experiment", :project_id => project.id, :slot => free_slot, :genetic_parts => project.genetic_parts.to_json
+    # puts req
+
+    # compare req and save the slot on the project 
+  end
+}
+
+every(1.minute, 'Trying to take a picture') {
+  # take picture with Project.active.last
+  # change the Project model to update the status of project to completed after 3 pictures
+  # ...
+  project = Project.active.last
+  unless project.nil?
+    puts "making rest call to take the picture :project_id=> #{project.id}, :slot => #{project.slot}"
+  end
 }
