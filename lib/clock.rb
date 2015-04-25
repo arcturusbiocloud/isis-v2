004e94803cf6d4568e5fa95b01c463adfee1f28a
luisbebop <luisbebop@gmail.com>
2015-04-24 20:48:13 -0700

    Added some TODO comments.

--- lib/clock.rb
+++ lib/clock.rb
@@ -16,6 +16,7 @@ every(1.minute, 'Trying to run experiment') {
     free_slot = Project.free_slot
 
     # making the rest call to run the experiment
+    # TODO: change the rest call to the real experiment endpoint
     puts "making rest call to run the experiment :project_id => #{project.id}, :slot => #{free_slot}, :genetic_parts => #{project.genetic_parts.to_json}"
     req = RestClient.post "http://arcturus:huxnGrbNfQFR@#{url}/api/run_virtual_experiment", :project_id => project.id, :slot => free_slot, :genetic_parts => project.genetic_parts.to_json
     # compare req and save the slot on the project 
@@ -33,6 +34,7 @@ every(1.minute, 'Trying to run experiment') {
 
 every(1.minute, 'Trying to take a picture') {
   # TODO: change the Project model to update the status of project to completed after 3 pictures
+  # TODO: make the real rest call to take the picture
   project = Project.active.last
   unless project.nil?
     puts "making rest call to take the picture :project_id=> #{project.id}, :slot => #{project.slot}"
