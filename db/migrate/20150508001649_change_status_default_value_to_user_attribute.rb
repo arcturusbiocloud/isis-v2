bd7444700abe8d1b8ad0801771bc4eb905d1565c
luisbebop <luisbebop@gmail.com>
2015-05-07 17:22:59 -0700

    Changed default User status to active.

---
+++ db/migrate/20150508001649_change_status_default_value_to_user_attribute.rb
@@ -0,0 +1,5 @@
+class ChangeStatusDefaultValueToUserAttribute < ActiveRecord::Migration
+  def change
+    change_column :users, :status, :integer, :default => 1
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422233607) do
+ActiveRecord::Schema.define(version: 20150508001649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20150422233607) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.string   "username",                            null: false
-    t.integer  "status",                 default: 0,  null: false
+    t.integer  "status",                 default: 1,  null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
