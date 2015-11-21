b55d096f55cde8349ad1e9effdc758f6d816c1d5
luisbebop <luisbebop@gmail.com>
2015-11-21 10:09:19 +0200

    Added FASTA file support and changed labels mentioning just GenBank files.

--- app/uploaders/gen_bank_uploader.rb
+++ app/uploaders/gen_bank_uploader.rb
@@ -26,7 +26,7 @@ class GenBankUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(gb)
+    %w(gb fasta)
   end
 
   # Override the filename of the uploaded files:
