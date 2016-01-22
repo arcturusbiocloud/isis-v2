0f8ee7a9114b82cba8ae186a26efdca06ab19b25
Bruno Freitas <bmsatierf@gmail.com>
2016-01-22 19:00:40 -0200

    New landing page tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,14 @@ class ApplicationController < ActionController::Base
     current_user.try(:admin?)
   end
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || projects_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   protected
 
   def configure_permitted_parameters
