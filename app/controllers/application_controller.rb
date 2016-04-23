1f31a3dcd0fe4ade9bc0b3cdf0982a241284f386
luisbebop <luisbebop@gmail.com>
2016-04-23 13:41:23 +0200

    Change the redirection path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || projects_path
+    stored_location_for(resource) || username_projects_path(current_user.username)
   end
 
   def after_sign_out_path_for(resource_or_scope)
